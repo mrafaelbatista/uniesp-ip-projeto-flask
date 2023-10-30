f36e12dda834c39dba2c24a1123faa3ec809290f
Messias R. Batista (Messi) <mrafaelbatista@gmail.com>
2023-10-29 22:41:41 -0300

    Adicionando variáveis de ambiente para ativação do debug;

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Definindo a variável de ambiente
+os.environ['FLASK_DEBUG'] = 'True'
+
+# Configurando o modo de depuração com base na variável de ambiente
+app.debug = os.environ.get('FLASK_DEBUG') == 'True'
 
 @app.route('/')
 def ola():
     return render_template('index.html')
+
 
 @app.route('/sobre-equipe')
 def sobre():
     return render_template('sobre.html')
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
